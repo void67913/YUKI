ca39885aaf9917055cae27599c282c1c54bdcbd7
void67913 <ohiedits0@gmail.com>
2026-09-20 18:50:23 +0600

    Add AFK command to set user status

--- src/commands/Utility/afk.js
+++ src/commands/Utility/afk.js
@@ -1,1 +1,29 @@
+import { SlashCommandBuilder } from 'discord.js';
 
+export default {
+  data: new SlashCommandBuilder()
+    .setName('afk')
+    .setDescription('Set your AFK status')
+    .addStringOption(option =>
+      option
+        .setName('reason')
+        .setDescription('Why are you AFK?')
+        .setRequired(false)
+    ),
+
+  async execute(interaction) {
+    const reason =
+      interaction.options.getString('reason') || 'AFK';
+
+    interaction.client.afkUsers ??= new Map();
+
+    interaction.client.afkUsers.set(interaction.user.id, {
+      reason,
+      timestamp: Date.now()
+    });
+
+    await interaction.reply(
+      `💤 **${interaction.user.username}** is now AFK!\n> ${reason}`
+    );
+  }
+};
